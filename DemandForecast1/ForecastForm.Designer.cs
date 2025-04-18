05cd9a2488d512652ab9626096c45cb244c2b9fb
mighiky <mighiky@MIGHIKY>
2025-04-18 13:39:11 +0300

    Добавьте файлы проекта.

---
+++ DemandForecast1/ForecastForm.Designer.cs
@@ -0,0 +1,29 @@
+﻿namespace DemandForecast // ← Исправил!
+{
+    partial class ForecastForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ForecastForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ForecastForm";
+            this.Text = "ForecastForm";
+            this.Load += new System.EventHandler(this.ForecastForm_Load);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ DemandForecast1/ForecastForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+
+namespace DemandForecast // ← Исправил!
+{
+    public partial class ForecastForm : Form
+    {
+        public ForecastForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ForecastForm_Load(object sender, EventArgs e)
+        {
+            var plotView = new PlotView
+            {
+                Dock = DockStyle.Fill
+            };
+
+            var model = new PlotModel { Title = "Прогноз спроса по месяцам" };
+
+            var lineSeries = new LineSeries
+            {
+                Title = "Спрос",
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.Blue
+            };
+
+            lineSeries.Points.Add(new DataPoint(0, 100));
+            lineSeries.Points.Add(new DataPoint(1, 120));
+            lineSeries.Points.Add(new DataPoint(2, 150));
+            lineSeries.Points.Add(new DataPoint(3, 170));
+            lineSeries.Points.Add(new DataPoint(4, 200));
+
+            model.Series.Add(lineSeries);
+            plotView.Model = model;
+
+            this.Controls.Add(plotView);
+        }
+    }
+}
